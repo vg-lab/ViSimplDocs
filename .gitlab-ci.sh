5251450d89f8fff78ec95ba4dd334a586d154842
Felix de las Pozas <felixdelaspozas@gmail.com>
2021-04-12 16:48:33 +0200

    Updated readme. Script for gitlab runner.

---
+++ .gitlab-ci.sh
@@ -0,0 +1,8 @@
+if [ ! -z "${GITHUB_MIRROR_URL}" ]; then
+    rm -rf ___GITLAB_REPO___
+    git clone git@`git remote -v | grep origin | cut -d "@" -f2 | sed -e "s/\//:/" | cut -d" " -f1 | head -n1` ___GITLAB_REPO___
+    cd ___GITLAB_REPO___
+    git remote add github ${GITHUB_MIRROR_URL}
+    git push github master
+fi
+
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Documentation and runbook of [ViSimpl](https://github.com/vg-lab/visimpl).
 
+## Requirements
+
+To build the documentation [Sphinx](https://www.sphinx-doc.org/en/master/) is required. 
