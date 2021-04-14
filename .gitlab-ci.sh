46966f19d3999d554a283ac4e8baa18acad2c4b5
Felix de las Pozas <felixdelaspozas@gmail.com>
2021-04-14 06:46:03 +0200

    Modified gitlab script.

--- .gitlab-ci.sh
+++ .gitlab-ci.sh
@@ -1,3 +1,4 @@
+set -e
 if [ ! -z "${GITHUB_MIRROR_URL}" ]; then
     rm -rf ___GITLAB_REPO___
     git clone git@`git remote -v | grep origin | cut -d "@" -f2 | sed -e "s/\//:/" | cut -d" " -f1 | head -n1` ___GITLAB_REPO___
