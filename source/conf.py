6f1635f0435820ce536ec6ee5c5747e91eb3b0b6
Felix de las Pozas <felixdelaspozas@gmail.com>
2022-06-16 16:05:13 +0200

    Update to version 1.7.3.

--- source/conf.py
+++ source/conf.py
@@ -24,7 +24,7 @@ author = 'Sergio E. Galindo & Félix de las Pozas Álvarez'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.7.2'
+release = '1.7.3'
 
 # -- General configuration ---------------------------------------------------
 
@@ -88,7 +88,7 @@ latex_maketitle = r'''
 \begin{center}
   \includegraphics[width=6cm,height=6cm]{visimpl-icon-square.png}\\[8ex]
   {\Huge SimPart \& StackViz Documentation}\\[4ex]
-  {\Large Version 1.7.2}\\[4ex]
+  {\Large Version 1.7.3}\\[4ex]
   {\Large Sergio E. Galindo \& F\'{e}lix de las Pozas \'{A}lvarez}\\[4ex]
   {\Large May 2022}\\[16ex]
   {\Large Visualization \& Graphics Lab, Universidad Rey Juan Carlos}\\[4ex]
